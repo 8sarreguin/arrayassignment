6035966ac00f9cc552b5ee9363a00ead9cc0085c
Shawn Arreguin <31480987+8sarreguin@users.noreply.github.com>
2017-09-28 09:07:50 -0500

    Add files via upload

--- forloopandarray.js
+++ forloopandarray.js
@@ -5,7 +5,16 @@ let speeds = [93,99,95,96,97,99];
 let pings = [45,60,120,80,155,160,220,110];
 let bits = [1,1,0,1,0,0,1,0,0,1];
 
-function example1(){
+function example1(list, amount){
+
+    let newOz = [];
+
+    for(let z = 0; z < list.length; z++){
+        if(list[z] >= amount){
+            newOz.push(list[z]);
+        }
+    }
+    return newOz;
 
 }
 
@@ -20,15 +29,25 @@ function example2(){
 
 function example3(){
 
+    let avg = 0;
+        for(p=0; p<pings.length; p++){
+            avg=avg+pings[p];
+        }
+        return avg/pings.length;
+
 }
 
 function example4(){
-
+    for(s=0; s<speeds.length; s++){
+        if(speeds[s]>95){
+            return speeds[s]
+        }
+    }
 }
 
 //Creating test code
 function main(){
-    console.log(example1());
+    console.log(example1(oz, 20));
     console.log("The total # of bits is "+example2());
     console.log("Your average ping for internet is "+example3());
     console.log("The first pitch thrown above 95 was pitch-index "+example4());
