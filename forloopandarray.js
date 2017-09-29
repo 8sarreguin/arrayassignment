f1eee534fb66a6f159371e0613d476774a8aafa9
Shawn Arreguin <31480987+8sarreguin@users.noreply.github.com>
2017-09-29 11:05:13 -0500

    Add files via upload

--- forloopandarray.js
+++ forloopandarray.js
@@ -5,6 +5,32 @@ let speeds = [93,99,95,96,97,99];
 let pings = [45,60,120,80,155,160,220,110];
 let bits = [1,1,0,1,0,0,1,0,0,1];
 
+let turtle = [16, 71, 101, 111, 118];
+let tadpole = [10240, 10481, 254, 1625, 2056];
+
+function adding(){
+
+    let tetra = [];
+
+    for(let t = 0; t< turtle.length; t++){
+        let x = tadpole[t] + turtle[t];
+        tetra.push(x)
+    }
+        return tetra;
+}
+
+function glue(){
+
+    let thingy = [];
+    for(let g = 0; g < turtle.length;g++){
+        thingy.push(turtle[g]);
+    }
+    for(let h = 0; h < turtle.length;h++){
+        thingy.push(tadpole[h]);
+    }
+    return thingy;
+}
+
 function example1(list, amount){
 
     let newOz = [];
@@ -51,6 +77,8 @@ function main(){
     console.log("The total # of bits is "+example2());
     console.log("Your average ping for internet is "+example3());
     console.log("The first pitch thrown above 95 was pitch-index "+example4());
+    console.log(adding());
+    console.log(glue());
 }
 //run test code with CTRL + SHIFT + B
 main();
